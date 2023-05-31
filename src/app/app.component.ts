1a8563d8902ba8b7f09ccb225454b7197bd3f52d
leslieMata <lesliemarx1@gmail.com>
2023-05-30 19:25:42 -0600

    peticiones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { articulo } from './interface/articulos.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'peticiones';
+    public  articulos : articulo[] = [];
+    constructor(private http: HttpClient) { }
+    ngOnInit(){
+        let url = "http://localhost/Front-End/unidad5/productos/";
+        this.http.get<articulo[]>(url)
+            .subscribe(
+                resultado =>{
+                    this.articulos = resultado;
+                    this.existeDatos = this.articulos.length > 0;
+                    console.log(this.articulos);
+                }
+            )
+    }
+    public existeDatos: boolean = false;
+    
 }
